224ddf62e12cb6bbb932a61a22645d06e3a7b1da
Judalm Park (dami) <wnekfa1004@naver.com>
2025-01-01 22:18:24 +0900

    [feat] 프로젝트 Configuration 및 테스트 컨테이너 환경 구성

---
+++ src/main/java/io/dami/market/infra/core/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package io.dami.market.infra.core.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("E 커머스 서비스 프로젝트 API Document")
+                .version("v1.0.0")
+                .description("E 커머스 서비스 API 명세서입니다.");
+        return new OpenAPI().info(info);
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("market")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
--- src/test/java/io/dami/market/TestcontainersConfiguration.java
+++ src/test/java/io/dami/market/TestcontainersConfiguration.java
@@ -1,25 +1,31 @@
 package io.dami.market;
 
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.GenericContainer;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Configuration;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
-@TestConfiguration(proxyBeanMethods = false)
+@Configuration
 class TestcontainersConfiguration {
 
-    @Bean
-    @ServiceConnection
-    MySQLContainer<?> mysqlContainer() {
-        return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+    public static final MySQLContainer<?> MYSQL_CONTAINER;
+
+    static {
+        MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
+                .withDatabaseName("hhplus")
+                .withUsername("test")
+                .withPassword("test");
+        MYSQL_CONTAINER.start();
+
+        System.setProperty("spring.datasource.url", MYSQL_CONTAINER.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC");
+        System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
+        System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
     }
 
-    @Bean
-    @ServiceConnection(name = "redis")
-    GenericContainer<?> redisContainer() {
-        return new GenericContainer<>(DockerImageName.parse("redis:latest")).withExposedPorts(6379);
+    @PreDestroy
+    public void preDestroy() {
+        if (MYSQL_CONTAINER.isRunning()) {
+            MYSQL_CONTAINER.stop();
+        }
     }
-
 }
