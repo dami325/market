e5fe358a6d393fa083d3bbdc862e31a0cb4b6323
Judalm Park (dami) <wnekfa1004@naver.com>
2025-01-10 03:00:00 +0900

    [feat] 유저, 포인트 통합 테스트 작성

---
+++ src/test/java/io/dami/market/application/point/PointServiceIntegrationTest.java
@@ -0,0 +1,47 @@
+package io.dami.market.application.point;
+
+import io.dami.market.domain.user.User;
+import io.dami.market.domain.user.UserRepository;
+import io.dami.market.utils.IntegrationServiceTest;
+import io.dami.market.utils.fixture.UserFixture;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+
+class PointServiceIntegrationTest extends IntegrationServiceTest {
+
+    @Autowired
+    private PointService pointService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void 포인트_조회_성공() {
+        // given
+        User user = userRepository.save(UserFixture.user("박주닮", 5000));
+
+        // when
+        BigDecimal balance = pointService.getBalance(user.getId());
+
+        // then
+        Assertions.assertThat(balance.compareTo(userRepository.getUser(user.getId()).getUserPoint().getBalance())).isEqualTo(0);
+    }
+
+    @Test
+    void 포인트_충전_성공() {
+        // given
+        User user = userRepository.save(UserFixture.user("박주닮", 5000));
+
+        // when
+        pointService.chargePoint(user.getId(), BigDecimal.valueOf(12000));
+
+        // then
+        User result = userRepository.getUser(user.getId());
+        Assertions.assertThat(result.getUserPoint().getBalance().compareTo(BigDecimal.valueOf(17000))).isEqualTo(0);
+    }
+
+}
