0070fe7105cbe22e7cc25e60c95c3c8cb2a05877
Judalm Park (dami) <wnekfa1004@naver.com>
2025-01-24 04:21:45 +0900

    [feat] 포인트 충전 분산락 적용 및 테스트

--- src/main/java/io/dami/market/domain/point/PointService.java
+++ src/main/java/io/dami/market/domain/point/PointService.java
@@ -1,34 +1,33 @@
 package io.dami.market.domain.point;
 
-import io.dami.market.domain.user.User;
-import io.dami.market.domain.user.UserRepository;
+import io.dami.market.infra.redis.redisson.DistributedLock;
+import java.math.BigDecimal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
 public class PointService {
 
-    private final PointRepository pointRepository;
+  private final PointRepository pointRepository;
 
-    @Transactional(readOnly = true)
-    public BigDecimal getBalance(Long userId) {
-        return pointRepository.getPointByUserId(userId)
-                .getTotalPoint();
-    }
+  @Transactional(readOnly = true)
+  public BigDecimal getBalance(Long userId) {
+    return pointRepository.getPointByUserId(userId)
+        .getTotalPoint();
+  }
 
-    @Transactional
-    public void chargePoint(Long userId, BigDecimal amount) {
-        pointRepository.getPointByUserId(userId)
-                .charge(amount);
-    }
+  @DistributedLock(key = "'POINT_' + #userId")
+  @Transactional
+  public void chargePoint(Long userId, BigDecimal amount) {
+    pointRepository.getPointByUserId(userId)
+        .charge(amount);
+  }
 
-    @Transactional
-    public void usePoints(Long userId, BigDecimal totalAmount) {
-        Point point = pointRepository.getPointByUserIdWithLock(userId);
-        point.subtract(totalAmount);
-    }
+  @Transactional
+  public void usePoints(Long userId, BigDecimal totalAmount) {
+    pointRepository.getPointByUserIdWithLock(userId)
+        .subtract(totalAmount);
+  }
 }
