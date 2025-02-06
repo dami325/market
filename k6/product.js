3dbbe6cd6bb023321428852c87ccc73c55b4a678
Judalm Park (dami) <wnekfa1004@naver.com>
2025-02-06 23:30:32 +0900

    [feat] 상위 상품 조회 k6 테스트 스크립트 추가

---
+++ k6/product.js
@@ -0,0 +1,14 @@
+import http from "k6/http";
+
+export let options = {
+  vus: 100,
+  duration: "10s",
+  iterations: 100,
+};
+
+const BASE_URL = "http://localhost:8080/api/v1/products";
+
+export default function () {
+  // 상위 상품 조회 API 요청
+  let response = http.get(`${BASE_URL}/rank`);
+}
