bc40c3d92cd4df8ad494bfe9b0f86f8bcfb90be2
Judalm Park (dami) <wnekfa1004@naver.com>
2025-02-07 03:29:42 +0900

    [feat] 기존 상위 상품 조회 테스트 제거

--- src/test/java/io/dami/market/domain/product/ProductServiceIntegrationTest.java
+++ src/test/java/io/dami/market/domain/product/ProductServiceIntegrationTest.java
@@ -3,9 +3,7 @@ package io.dami.market.domain.product;
 import io.dami.market.application.order.OrderFacade;
 import io.dami.market.application.payment.PaymentFacade;
 import io.dami.market.domain.order.Order;
-import io.dami.market.domain.order.OrderCommand;
 import io.dami.market.domain.order.OrderRepository;
-import io.dami.market.domain.point.Point;
 import io.dami.market.domain.point.PointRepository;
 import io.dami.market.domain.user.User;
 import io.dami.market.domain.user.UserRepository;
@@ -13,10 +11,8 @@ import io.dami.market.infra.product.ProductJpaRepository;
 import io.dami.market.interfaces.product.ProductResponse;
 import io.dami.market.utils.IntegrationServiceTest;
 import io.dami.market.utils.fixture.OrderFixture;
-import io.dami.market.utils.fixture.PointFixture;
 import io.dami.market.utils.fixture.ProductFixture;
 import io.dami.market.utils.fixture.UserFixture;
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -115,48 +111,6 @@ class ProductServiceIntegrationTest extends IntegrationServiceTest {
     Assertions.assertThat(result).isNotNull();
     Assertions.assertThat(result.getName()).isEqualTo("좋은데이");
     Assertions.assertThat(result.getStockQuantity()).isEqualTo(100);
-  }
-
-  @Test
-  void 탑5_상품_조회_성공() {
-    // given
-    Product productA = productRepository.save(ProductFixture.product("진로", 10000));
-    Product productB = productRepository.save(ProductFixture.product("좋은데이", 20000));
-    Product productC = productRepository.save(ProductFixture.product("참이슬", 20000));
-    Product productD = productRepository.save(ProductFixture.product("카스", 20000));
-    Product productE = productRepository.save(ProductFixture.product("처음처럼", 20000));
-    Product productF = productRepository.save(ProductFixture.product("테라", 20000));
-    User user = userRepository.save(UserFixture.user("박주닮"));
-    Point point = pointRepository.save(PointFixture.point(user.getId(), 10000000));
-    int maxQuantity = 50;
-    int minQuantity = 2;
-    List<OrderCommand.ProductStock> productStocks = List.of(
-        new OrderCommand.ProductStock(productA.getId(), minQuantity),
-        new OrderCommand.ProductStock(productB.getId(), 20),
-        new OrderCommand.ProductStock(productC.getId(), 35),
-        new OrderCommand.ProductStock(productD.getId(), 45),
-        new OrderCommand.ProductStock(productE.getId(), maxQuantity),
-        new OrderCommand.ProductStock(productF.getId(), 1)
-    );
-    OrderCommand.CreateOrder commend = new OrderCommand.CreateOrder(user.getId(), null,
-        productStocks);
-    Order order = orderFacade.createOrder(commend);
-    paymentFacade.processOrderPayment(user.getId(), order.getId());
-
-    // when
-    List<ProductResponse.Top5ProductDetails> result = productService.getProductsTop5();
-
-    // then
-    Assertions.assertThat(result).isNotEmpty();
-    Assertions.assertThat(result.size()).isEqualTo(5);
-    int maxSaleCount = 0;
-    int minSaleCount = Integer.MAX_VALUE;
-    for (ProductResponse.Top5ProductDetails productDetails : result) {
-      maxSaleCount = Math.max(maxSaleCount, productDetails.total_quantity_sold());
-      minSaleCount = Math.min(minSaleCount, productDetails.total_quantity_sold());
-    }
-    Assertions.assertThat(maxSaleCount).isEqualTo(maxQuantity);
-    Assertions.assertThat(minSaleCount).isEqualTo(minQuantity);
   }
 
   @DisplayName("주문 상품 재고 부족 차감 실패 ProductIsOutOfStock 발생")
