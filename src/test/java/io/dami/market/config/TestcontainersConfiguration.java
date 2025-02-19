078b0aa6420233226d286ed207b291a385ba8f22
Judalm Park (dami) <wnekfa1004@naver.com>
2025-02-20 00:22:24 +0900

    [feat] 카프카 애플리케이션 연동 통합 테스트 환경 구축 및 메시지 발행 로그 확인

--- src/test/java/io/dami/market/config/TestcontainersConfiguration.java
+++ src/test/java/io/dami/market/config/TestcontainersConfiguration.java
@@ -5,48 +5,61 @@ import org.springframework.context.annotation.Configuration;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @Configuration
 public class TestcontainersConfiguration {
 
-    public static final MySQLContainer<?> MYSQL_CONTAINER;
+  public static final MySQLContainer<?> MYSQL_CONTAINER;
 
-    private static final int REDIS_PORT = 6379;
-    private static final String REDIS_IMAGE = "redis:latest";
-    private static final String REDIS_TEST_PASSWORD = "testpassword";
+  private static final int REDIS_PORT = 6379;
+  private static final String REDIS_IMAGE = "redis:latest";
+  private static final String REDIS_TEST_PASSWORD = "testpassword";
 
-    public static final GenericContainer REDIS;
+  public static final GenericContainer REDIS;
 
-    static {
-        MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
-                .withDatabaseName("hhplus")
-                .withUsername("test")
-                .withPassword("test");
-        MYSQL_CONTAINER.start();
+  public static final ConfluentKafkaContainer KAFKA;
 
-        System.setProperty("spring.datasource.url", MYSQL_CONTAINER.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC");
-        System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
-        System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
+  static {
+    MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
+        .withDatabaseName("hhplus")
+        .withUsername("test")
+        .withPassword("test");
+    MYSQL_CONTAINER.start();
 
-        REDIS = new GenericContainer(DockerImageName.parse(REDIS_IMAGE))
-                .withExposedPorts(REDIS_PORT)
-                .withCommand("redis-server", "--requirepass", REDIS_TEST_PASSWORD)
-                .waitingFor(Wait.forListeningPort());
+    System.setProperty("spring.datasource.url",
+        MYSQL_CONTAINER.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC");
+    System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
+    System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
 
-        REDIS.start();
-        System.setProperty("spring.data.redis.host", REDIS.getHost());
-        System.setProperty("spring.data.redis.port", String.valueOf(REDIS.getFirstMappedPort()));
-        System.setProperty("spring.data.redis.password", REDIS_TEST_PASSWORD);
+    REDIS = new GenericContainer(DockerImageName.parse(REDIS_IMAGE))
+        .withExposedPorts(REDIS_PORT)
+        .withCommand("redis-server", "--requirepass", REDIS_TEST_PASSWORD)
+        .waitingFor(Wait.forListeningPort());
+
+    REDIS.start();
+    System.setProperty("spring.data.redis.host", REDIS.getHost());
+    System.setProperty("spring.data.redis.port", String.valueOf(REDIS.getFirstMappedPort()));
+    System.setProperty("spring.data.redis.password", REDIS_TEST_PASSWORD);
+
+    KAFKA = new ConfluentKafkaContainer("confluentinc/cp-kafka:latest");
+    KAFKA.start();
+    System.setProperty("spring.kafka.bootstrap-servers", KAFKA.getBootstrapServers());
+
+
+  }
+
+  @PreDestroy
+  public void preDestroy() {
+    if (MYSQL_CONTAINER.isRunning()) {
+      MYSQL_CONTAINER.stop();
     }
-
-    @PreDestroy
-    public void preDestroy() {
-        if (MYSQL_CONTAINER.isRunning()) {
-            MYSQL_CONTAINER.stop();
-        }
-        if (REDIS.isRunning()) {
-            REDIS.stop();
-        }
+    if (REDIS.isRunning()) {
+      REDIS.stop();
     }
+    if (KAFKA.isRunning()) {
+      KAFKA.stop();
+    }
+  }
 }
